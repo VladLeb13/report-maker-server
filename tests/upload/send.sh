d631008dd8ae85ed8b5d359b2001806d75a2546a
Vladislav Lebedev <lebedev@1c-connect.com>
2021-01-06 02:23:46 +0500

    Тест на данных от клиентской части

--- tests/upload/send.sh
+++ tests/upload/send.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-cat structs.json | curl -X POST --data @- -H "Content-Type: application/json" -H "Authorization: Basic Y2xpZW50OmNsaWVudHBhc3M=" http://localhost:8080/upload
+#cat structs.json | curl -X POST --data @- -H "Content-Type: application/json" -H "Authorization: Basic Y2xpZW50OmNsaWVudHBhc3M=" http://localhost:8080/upload
 #cat structs.json | curl -X POST --data @- -H "Content-Type: application/json" -H "Authorization: Basic Y2xpZW50czpjbGllbnRwYXNz" http://localhost:8080/upload
 #cat structs.json | curl -X POST --data @- -H "Content-Type: application/json"  http://localhost:8080/upload
+##### test
+
+cat example.json | curl -X POST --data @- -H "Content-Type: application/json" -H "Authorization: Basic Y2xpZW50OmNsaWVudHBhc3M=" http://localhost:8080/upload
