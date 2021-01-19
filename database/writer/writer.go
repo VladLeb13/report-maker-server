0271faabb78dee1d79c63d43ed82f1e0109acbab
Lebedev Vladislav <lebedev@1c-connect.com>
2021-01-19 13:20:32 +0500

    туда же

--- database/writer/writer.go
+++ database/writer/writer.go
@@ -18,25 +18,25 @@ func AddRecord(data interface{}) {
 	}
 }
 
-func fromRreiver(data *datalib.Report) {
-	//TODO: write to db report
+func fromRreiver(report *datalib.Report) {
+	data := datalibToModel(*report)
 
-	log.Printf("%+v", data)
+	fromApp(data)
+}
+
+func datalibToModel(report datalib.Report) (data model.TO_WR) {
+	data.Workstation.Name = report.Software.OS.Name
+	data.Workstation.Comment = "buh"
+	data.Workstation.Allow_analysis = 0
+
+	data.Workstation.Hardware = fill.Hardware(report)
+	data.Workstation.Program_list = fill.Program(report)
+	data.Workstation.Fault_tolerance = fill.Fault_tolerance(report)
+	data.Workstation.Perfomance = fill.Perfomance(report)
+
+	return
 }
 
 func fromApp(data model.TO_WR) {
-
-}
-
-func datalibToModel(report datalib.Report) (out model.TO_WR) {
-	out.Workstation.Name = report.Software.OS.Name
-	out.Workstation.Comment = "buh"
-	out.Workstation.Allow_analysis = 0
-
-	out.Workstation.Hardware = fill.Hardware(report)
-	out.Workstation.Program_list = fill.Program(report)
-	out.Workstation.Fault_tolerance = fill.Fault_tolerance(report)
-	out.Workstation.Perfomance = fill.Perfomance(report)
-
-	return
+	//TODO: write to db report
 }
