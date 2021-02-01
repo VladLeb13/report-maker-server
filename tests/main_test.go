48527d19d852f46252f7c8483ac0a64b8708784b
VladLeb13 <vlad194127@gmail.ru>
2021-02-01 16:56:13 +0500

    Test for workers

--- tests/main_test.go
+++ tests/main_test.go
@@ -5,13 +5,16 @@ import (
 	"log"
 	"testing"
 
+	"report-maker-server/classifier"
 	"report-maker-server/config"
 	"report-maker-server/database"
 	"report-maker-server/server"
 	"report-maker-server/tools"
+
+	class "github.com/VladLeb13/classifier/run"
 )
 
-func TestApp(t *testing.T) {
+func TestServe(t *testing.T) {
 	ctx := tools.AppContex{
 		Context: context.Background(),
 	}
@@ -31,4 +34,37 @@ func TestApp(t *testing.T) {
 	if err != nil {
 		log.Println(err)
 	}
+}
+
+func TestStartWorker(t *testing.T) {
+
+	ctx := tools.AppContex{
+		Context: context.Background(),
+	}
+	perfStruct := tools.PerformanceAnalyzerStruct{
+		Data:   make(chan class.Data),
+		Result: make(chan []string),
+	}
+	faultTlStruct := tools.FaultTolerantAnalyzerStruct{
+		Data:   make(chan tools.FaultTolerantParameters),
+		Result: make(chan float64),
+	}
+	//Используются внутри воркеров
+	ctx.Context = context.WithValue(ctx.Context, "PerformanceAnalyzerStruct", perfStruct)
+	ctx.Context = context.WithValue(ctx.Context, "FaultTolerantAnalyzerStruct", faultTlStruct)
+
+	perfChan := tools.PerfomanceChan{
+		DataForPerformanceAnalyze: make(chan tools.DataForPerformanceAnalyze),
+		AnalysisResult:            make(chan tools.PerformanceAnalysisResult),
+	}
+	ctx.Context = context.WithValue(ctx.Context, "PerfomanceChan", perfChan)
+
+	faultTlChan := tools.FaultTolerantChan{
+		DataFaultTolerantAnalyze: make(chan tools.FaultTolerantParameters),
+		AnalysisResult:           make(chan float64),
+	}
+	ctx.Context = context.WithValue(ctx.Context, "FaultTolerantChan", faultTlChan)
+
+	classifier.Manager(&ctx)
+
 }
